7fba9bbd553d3a7510d3c65aa12e2e61a8a6fda0
Sebastian Willert <willert@gmail.com>
2011-03-12 01:16:59 +0100

    Ignore some well-known warnings from App::FatPacker

--- script/pack.sh
+++ script/pack.sh
@@ -5,7 +5,15 @@ perl -Mlocal::lib=perl5 -Ilib `which fatpack` packlists-for `cat fatpacker.trace
 echo Build tree
 perl -Mlocal::lib=perl5 -Ilib `which fatpack` tree `cat packlists`
 echo Fat-packing
-( echo '#!/usr/bin/perl'; perl -Mlocal::lib=perl5 -Ilib `which fatpack` file; cat script/mist.PL ) > bin/mist
+(
+		echo '#!/usr/bin/perl' >&2;
+		perl -Mlocal::lib=perl5 -Ilib `which fatpack` file 3>&1 1>&2 2>&3 |
+		  grep -v "\.pod isn't a .pm file" |
+		  grep -v "auto/List/Util/Util" |
+		  grep -v "auto/Params/Util/Util" |
+			cat
+    cat script/mist.PL | grep -v '^#' >&2
+) 2> bin/mist
 
 rm fatpacker.trace
 rm packlists
