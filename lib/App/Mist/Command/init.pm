3fadbea8ea971f3a2a409e9f119391d20f74c9e7
Sebastian Willert <willert@gmail.com>
2011-04-04 19:24:20 +0200

    Added minimal init command

---
+++ lib/App/Mist/Command/init.pm
@@ -0,0 +1,25 @@
+package App::Mist::Command::init;
+
+use strict;
+use warnings;
+
+use App::Mist -command;
+
+use Try::Tiny;
+use File::Which;
+use File::Find::Upwards;
+use Path::Class qw/dir/;
+use Cwd;
+
+sub execute {
+  my ( $self, $opt, $args ) = @_;
+  my $app = $self->app;
+
+  $self->app->execute_command(
+    $app->prepare_command( 'inject', $app->run_dzil( 'listdeps' ))
+  );
+
+  return;
+}
+
+1;
