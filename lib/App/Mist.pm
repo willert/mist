6ddd23a13daf8b971c7b4f67fcd88a717a2338ec
Sebastian Willert <willert@gmail.com>
2011-04-04 19:04:41 +0200

    Streamlined gathering of project info (boy, do I miss builders)

--- lib/App/Mist.pm
+++ lib/App/Mist.pm
@@ -11,6 +11,7 @@ use Module::Pluggable search_path => [ 'App::Mist::Command' ];
 
 use Path::Class qw/dir file/;
 use File::HomeDir;
+use File::Which;
 use File::Find::Upwards;
 
 {
@@ -18,8 +19,10 @@ use File::Find::Upwards;
   sub project_root {
     my $self = shift;
     $project_root ||= do{
-      my $root = find_containing_dir_upwards( 'dist.ini' )
-        or die "Can't find project root";
+      my $root = eval{
+        local $SIG{__WARN__} = sub{};
+        find_containing_dir_upwards( 'dist.ini' )
+      } or die "$0: Can't find project root\n";
       dir( $root )->absolute->resolve;
     };
   }
@@ -44,11 +47,85 @@ use File::Find::Upwards;
   }
 }
 
-sub workspace_lib {
-  my $self = shift;
-  my $ll = $self->workspace->subdir('perl5');
-  $ll->mkpath;
-  return $ll;
+{
+  my $ws_lib;
+  sub workspace_lib {
+    my $self = shift;
+    $ws_lib ||= do{
+      my $ll = $self->workspace->subdir('perl5');
+      $ll->mkpath;
+      $ll;
+    };
+  }
 }
+
+{
+  my $mpan;
+  sub mpan_dist {
+    my $self = shift;
+    $mpan ||= $self->project_root->subdir(
+      $ENV{MIST_DIST_DIR}  || 'mpan-dist'
+    );
+  }
+
+  sub mpan_conf {
+    my $self = shift;
+    my $conf_dir = $self->mpan_dist->subdir('mist');
+    return @_ ? $conf_dir->file( @_ ) : $conf_dir;
+  }
+
+}
+
+{
+  my $local_lib;
+  sub local_lib {
+    my $self = shift;
+    $local_lib ||= $self->project_root->subdir(
+      $ENV{MIST_LOCAL_LIB} || 'perl5'
+    );
+  }
+}
+
+
+{
+  my $dzil;
+  sub dzil_executable {
+    my $self = shift;
+    $dzil ||= which( 'dzil' ) or die "$0: dzil not found\n";
+  }
+
+}
+
+{
+  my $cpanm_executable;
+  sub cpanm_executable {
+    my $self = shift;
+    $cpanm_executable ||= which( 'cpanm' )
+      or die "$0: cpanm not found\n";
+  }
+}
+
+sub load_cpanm {
+  my $self = shift;
+  my $pkg = $self->cpanm_executable;
+
+  do $pkg;
+  require App::cpanminus;
+
+  die "$0: cpanm v$App::cpanminus::VERSION is too old, v1.4 needed\n"
+    if $App::cpanminus::VERSION < 1.4;
+
+  return;
+}
+
+sub run_cpanm {
+  my ( $self, @cmd_opts ) = @_;
+  $self->load_cpanm;
+
+  my $app = App::cpanminus::script->new;
+  $app->parse_options( @cmd_opts );
+  $app->doit or exit(1);
+}
+
 
 1;
--- lib/App/Mist/Command/inject.pm
+++ lib/App/Mist/Command/inject.pm
@@ -13,23 +13,14 @@ use Cwd;
 
 use File::Temp qw/ tempfile tempdir /;
 
-
 sub execute {
   my ( $self, $opt, $args ) = @_;
 
-  die "No module to install" unless $args and ref $args eq 'ARRAY' and @$args;
+  die "$0: No module to install"
+    unless $args and ref $args eq 'ARRAY' and @$args;
 
-  my $cpanm = which( 'cpanm' )
-    or die "cpanm not found";
-
-  do $cpanm;
-  require App::cpanminus::script;
-
-  my $home      = $self->app->project_root;
-  my $mpan      = $home->subdir( $ENV{MIST_DIST_DIR}  || 'mpan-dist' );
-  my $local_lib = $home->subdir( $ENV{MIST_LOCAL_LIB} || 'perl5' );
-
-  my $mpan_conf = $mpan->subdir( 'mist' );
+  my $mpan      = $self->app->mpan_dist;
+  my $local_lib = $self->app->local_lib;
   my $workspace = $self->app->workspace_lib;
 
   my @options   = (
@@ -41,14 +32,7 @@ sub execute {
     "--save-dists=${mpan}",
   );
 
-  require Module::CoreList;
-  $Module::CoreList::version{$]+0} =
-    $Module::CoreList::version{5.008001};
-
-  my $app = App::cpanminus::script->new;
-  $app->parse_options( @options, @$args );
-  $app->doit or exit(1);
-
+  $self->app->run_cpanm( @options, @$args );
 }
 
 
