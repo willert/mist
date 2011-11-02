e6d273b412d7d62b808fbdf0f938774908d064ae
Sebastian Willert <willert@gmail.com>
2011-11-02 05:42:43 +0100

    Command to clean obsolete dists from mist env

---
+++ lib/App/Mist/Command/clean.pm
@@ -0,0 +1,58 @@
+package App::Mist::Command::clean;
+
+use strict;
+use warnings;
+
+use App::Mist -command;
+
+use CPAN::PackageDetails;
+
+use File::Find;
+use File::Find::Upwards;
+use Path::Class qw/dir file/;
+use Cwd;
+
+use version 0.74;
+
+use Data::Dumper::Concise;
+
+sub execute {
+  my ( $self, $opt, $args ) = @_;
+
+  my $home      = $self->app->project_root;
+  my $mpan      = $self->app->mpan_dist;
+  my $local_lib = $self->app->local_lib;
+
+  my $mpan_modules = $mpan->subdir( 'modules' );
+  my $mpan_authors = $mpan->subdir( 'authors' );
+
+  my $package_details = CPAN::PackageDetails->read(
+    $mpan_modules->file( '02packages.details.txt.gz' )->stringify
+  );
+
+  my $records = $package_details->entries;
+
+  my %dist_package;
+
+  print "Scanning package details ..\n", ;
+
+  while ( my ( $pkg, $versions ) = each %{ $records->entries } ) {
+    # printf qq{%s:\n  %s\n\n}, $pkg, join( q{, }, keys %$versions );
+    $dist_package{ $_->path } = 1 for values %$versions;
+  }
+
+  print "Removing obsolete distributions ..\n", ;
+
+  chdir $mpan->stringify;
+  find({ wanted => sub{
+    return unless -f $_;
+    my $from = dir( cwd() )->subdir( 'authors', 'id' );
+    ( my $path = $_ ) =~ s|${from}/||;
+    unlink $_ unless $dist_package{ $path };
+    # printf "%s\n", $path;
+  }, no_chdir => 1 }, $mpan_authors->stringify );
+
+
+}
+
+1;
