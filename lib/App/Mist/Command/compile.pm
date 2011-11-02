a32ad2572f8e9f9194f81cb8da2e97719453a8aa
Sebastian Willert <willert@gmail.com>
2011-11-02 05:36:27 +0100

    Also compile a command wrapper for bash

--- lib/App/Mist/Command/compile.pm
+++ lib/App/Mist/Command/compile.pm
@@ -216,6 +216,61 @@ INSTALLER
     rename "mpan-install.tmp", "mpan-install";
     chmod 0755, "mpan-install";
 
+    print STDERR "Generating cmd wrapper\n";
+
+
+    my $wrapper = $app->mpan_dist->file('cmd-wrapper.bash')->stringify;
+    open $out, ">", $wrapper or die $!;
+
+    print $out <<'CMD_WRAPPER';
+#!/bin/bash
+
+# get the absolute path of the executable
+SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH=$SELF_PATH/$(basename -- "$0")
+
+# resolve symlinks
+while [ -h $SELF_PATH ]; do
+    # 1) cd to directory of the symlink
+    # 2) cd to the directory of where the symlink points
+    # 3) get the pwd
+    # 4) append the basename
+    DIR=$(dirname -- "$SELF_PATH")
+    SYM=$(readlink $SELF_PATH)
+    SELF_PATH=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)/$(basename -- "$SYM")
+done
+
+WRAPPER=$(dirname $SELF_PATH)
+
+CALL_PATH="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/$(basename $0)"
+
+if [ $SELF_PATH == $CALL_PATH ] ; then
+  echo "$(basename $0): Must be called as a symlink named like the command to be run" >&2
+  exit 1
+fi
+
+# try to upward-find local::lib directory
+for UPDIR in . .. ../.. ../../.. ../../../.. ; do
+  TEST="$WRAPPER/$UPDIR/perl5";
+  if [ -d $TEST ] ; then
+      LOCAL_LIB=$( cd -P "$( dirname "$TEST" )" && pwd )
+      BASE_DIR="$WRAPPER/$UPDIR"
+      break
+  fi
+done
+
+#exit if we can't find any
+if [ ! $LOCAL_LIB ] ; then
+  echo "$0: No local::lib directory found. Abort!" >&2
+  exit 1
+fi
+
+$BASE_DIR/perl5/bin/mist-run `basename $0` $@
+
+CMD_WRAPPER
+
+    close $out;
+    chmod 0755, $wrapper;
+
   } catch {
     warn "$_\n";
   } finally {
