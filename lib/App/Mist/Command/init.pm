cea6f37ea2ad5a6f97aa29edaf45c95a5abe5f2f
Sebastian Willert <willert@gmail.com>
2012-02-23 20:41:38 +0100

    init now respects prepend and notest

--- lib/App/Mist/Command/init.pm
+++ lib/App/Mist/Command/init.pm
@@ -15,9 +15,30 @@ sub execute {
   my ( $self, $opt, $args ) = @_;
   my $app = $self->app;
 
+  my $dist_prepend = $self->app->mpan_conf->file(qw/ 01.prepend.txt /);
+  my $dist_notest  = $self->app->mpan_conf->file(qw/ 02.notest.txt /);
+
+  my @prepend = $self->app->slurp_file( $dist_prepend );
+  my @notest  = $self->app->slurp_file( $dist_notest );
+
+  my @prereqs = grep{
+    my $this = $_;
+    ! grep{
+      $this =~ m/^${_}(?:~.*)$/ and $_ = $this # pick up version string
+    } ( @prepend, @notest );
+  } $self->app->fetch_prereqs;
+
   $app->execute_command( $app->prepare_command(
-    'inject', '--skip-satisfied', $self->app->fetch_prereqs
-  ));
+    'inject', '--skip-satisfied', @prepend
+  )) if @prepend;
+
+  $app->execute_command( $app->prepare_command(
+    'inject', '--skip-satisfied', '--notest', @notest
+  )) if @notest;
+
+  $app->execute_command( $app->prepare_command(
+    'inject', '--skip-satisfied', @prereqs
+  )) if @prereqs;
 
   $app->execute_command( $app->prepare_command(
     'compile'
