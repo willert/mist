0ebf905bee9f75baddce93a2d483e535f135b8fc
Sebastian Willert <willert@gmail.com>
2012-12-20 04:15:07 +0100

    Try to force cpanm to favor our local mpan repository when instaing dependencies

--- lib/App/Mist/Command/inject.pm
+++ lib/App/Mist/Command/inject.pm
@@ -21,22 +21,54 @@ sub execute {
   die "$0: No module to install"
     unless $args and ref $args eq 'ARRAY' and @$args;
 
+  $self->app->ensure_correct_perlbrew_context;
+
   my $mpan      = $self->app->mpan_dist;
   my $local_lib = $self->app->local_lib;
   my $workspace = $self->app->workspace_lib;
 
-  my @options   = (
+  my @base_options   = (
     "--quiet",
     "--local-lib-contained=${local_lib}",
     "--mirror-only",
-    "--mirror=http://search.cpan.org/CPAN",
-    "--mirror=file://${mpan}",
     "--save-dists=${mpan}",
   );
 
+  # favor mpan-dist packages for dependencies
+  my @dependency_options = (
+    @base_options,
+    "--mirror=file://${mpan}",
+    "--mirror=http://search.cpan.org/CPAN",
+    "--installdeps",
+  );
+
+  # use cpan itself for the requested package itself
+  my @install_options = (
+    @base_options,
+    "--mirror=http://search.cpan.org/CPAN",
+  );
+
   my $installed_packages = 0;
+  my $initial_directory = cwd();
 
   $self->app->load_cpanm;
+
+  DOWNLOAD_DIST: {
+    my $guard = wrap 'App::cpanminus::script::build_stuff',
+      pre  => sub{
+        my ( $cpanm, $module, $dist ) = @_;
+        printf STDERR "Downloading: %s\n", $module;
+        $_[-1] = 1;             # shor-circuit call to prevent installation
+      };
+
+    try {
+      no warnings 'redefine';
+      *CORE::GLOBAL::exit = sub{};
+      $self->app->run_cpanm( @install_options, @$args );
+    } finally {
+      chdir $initial_directory;
+    };
+  }
 
   CPANM_AUTO_INDEXER: {
 
@@ -44,18 +76,24 @@ sub execute {
       pre  => sub{
         my ( $cpanm, $module, $dist ) = @_;
         printf STDERR "Building: %s\n", $module;
-
       },
       post => sub{
         my ( $cpanm, $module, $dist ) = @_;
-
-        if ( my $success = !! $_[-1]  ) {
+        if ( $dist and my $success = !! $_[-1]  ) {
           $self->app->add_distribution_to_index( $dist );
           $installed_packages += 1;
         }
       };
 
-    $self->app->run_cpanm( @options, @$args );
+    try {
+      no warnings 'redefine';
+      *CORE::GLOBAL::exit = sub{};
+      $self->app->run_cpanm( @dependency_options, @$args );
+    } finally {
+      chdir $initial_directory;
+    };
+
+    $self->app->run_cpanm( @install_options, @$args );
   }
 
   $self->app->commit_mpan_package_index if $installed_packages;
