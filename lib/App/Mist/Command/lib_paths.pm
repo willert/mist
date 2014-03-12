bd66ef76e90882ecdc3183dc7f9cfbc2316168c6
Sebastian Willert <willert@gmail.com>
2014-03-12 22:00:41 +0100

    Print full lib path incl. expanded local::libs

--- lib/App/Mist/Command/lib_paths.pm
+++ lib/App/Mist/Command/lib_paths.pm
@@ -2,12 +2,14 @@ package App::Mist::Command::lib_paths;
 use 5.010;
 
 use App::Mist -command;
+use local::lib 2.00;
 
 sub execute {
   my ( $self, $opt, $args ) = @_;
   my $ctx = $self->app->ctx;
 
-  say $_ for $ctx->project_root->subdir('lib'), $ctx->local_lib;
+  say $_ for $ctx->project_root->subdir('lib'),
+    local::lib->lib_paths_for( $ctx->local_lib );
 }
 
 1;
