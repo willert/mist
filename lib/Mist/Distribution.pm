785590e967933650e7d8c9be9f8a44b261c17b74
Sebastian Willert <willert@gmail.com>
2014-03-29 05:35:33 +0100

    Merge prepend, notest and cpanfile requirements
    This finally gives use notest for prepend, keeps all version information
    even if it comes from different sources (e.g. prepend now uses version
    requirements from cpanfile if no other is given).
    
    Also expose options in mpan-install to manipulate this composition.

--- lib/Mist/Distribution.pm
+++ lib/Mist/Distribution.pm
@@ -1,4 +1,6 @@
 package Mist::Distribution;
+# ABSTRACT: provides access to build parameters for this project
+
 use strict;
 use warnings;
 
@@ -28,7 +30,9 @@ sub perl ($) {
   $self->{perl} = $version;
 }
 
-sub get_default_perl_version { my $self = shift; return $self->{ perl }; }
+sub get_default_perl_version {
+  my $self = shift; return $self->{ perl };
+}
 
 sub prepend ($;$) {
   my ( $self, $module, $version ) = @_;
@@ -37,13 +41,91 @@ sub prepend ($;$) {
   push @{ $self->{prepend}}, $module;
 }
 
-sub get_prepended_modules { my $self = shift; return @{ $self->{ prepend }}; }
+sub get_prepended_modules {
+  my $self = shift;
+  return @{ $self->{ prepend }};
+}
 
 sub notest ($) {
   my ( $self, $module ) = @_;
   push @{ $self->{notest}}, $module;
 }
 
-sub get_modules_not_to_test { my $self = shift; return @{ $self->{ notest }}; }
+sub get_modules_not_to_test {
+  my $self = shift;
+  return @{ $self->{ notest }};
+}
+
+sub build_cpanm_call_stack {
+  my ( $self, @prereqs ) = @_;
+
+  my %opts;
+  %opts = %{ shift @prereqs }
+    if @prereqs and ref $prereqs[0] eq 'HASH';
+
+  @prereqs = @{ shift @prereqs }
+    if @prereqs == 1 and ref $prereqs[0] eq 'ARRAY';
+
+  my @prepended = $self->get_prepended_modules;
+
+  # state variables
+  my ( %version, %scheduled, %dont_test, @callstack );
+
+  # build hash of modules we don't want to test
+  %dont_test = map{ $_ => 1 } $self->get_modules_not_to_test;
+
+  # push a module on the call stack according to state vars
+  my $push_module_on_stack = sub{
+    my $module = shift;
+    return if $scheduled{ $module };
+
+    my $mod_spec = $version{ $module } ?
+      join( q{~}, $module, $version{$module} ) : $module;
+
+    if ( $dont_test{ $module } and not $opts{'force-tests'} ) {
+      push @callstack, [ '--installdeps', $mod_spec ];
+      push @callstack, [ '--notest', $mod_spec ];
+    } else {
+      push @callstack, [ $mod_spec ];
+    }
+
+    $scheduled{ $module } = 1;
+  };
+
+  # pre-parse version spec from prerequisites
+  for ( @prereqs ) {
+    my ( $module, $version ) = split( q{~}, $_, 2 );
+    $_ = $module;
+    $version{ $module } = $version;
+  }
+
+  # pre-parse version spec from prepended modules
+  for ( @prepended ) {
+    my ( $module, $version ) = split( q{~}, $_, 2 );
+    $_ = $module;
+
+    warn "Conflicting versions for $module in mistfile and painful\n"
+      if $version and $version{$module} and $version ne $version{$module};
+
+    # prepended version requirement superseded cpanfile requirement
+    $version{ $module } = $version if $version;
+  }
+
+  # schedule prepended modules
+  unless ( $opts{'skip-prepended'} ) {
+    $push_module_on_stack->( $_ ) for @prepended;
+  }
+
+  # schedule remaining modules that are not tested before the remaining
+  # prerequisites to avoid prereqs pulling in this module without --notest
+  unless ( $opts{'skip-notest'} ) {
+    $push_module_on_stack->( $_ ) for keys %dont_test;
+  }
+
+  # schedule remaining prerequisites
+  $push_module_on_stack->( $_ ) for @prereqs;
+
+  return @callstack;
+}
 
 1;
