79deeb1b3e5c3868a87254359a996187fd8848e7
Sebastian Willert <willert@gmail.com>
2026-07-27 06:05:25 +0200

    Checking in changes prior to tagging of version 0.48.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e9910a5..d902585 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for App::Mist
     
     {{$NEXT}}
     
    +0.48 2026-07-27T04:05:18Z
    +
       - mist merge: the mistfile block for a merged dist now binds only to a top-level
         `### <<<[Dist]` marker pair rather than the first one anywhere in the file, so a
         subordinate pair - one nested inside a sibling's merge block - no longer captures

--- lib/App/Mist.pm
+++ lib/App/Mist.pm
@@ -46,7 +46,7 @@ ERROR_MSG
 
 use App::Cmd::Setup -app;
 
-our $VERSION = '0.47';
+our $VERSION = '0.48';
 
 use App::Mist::Context;
 
--- lib/App/Mist/Util/StripPod.pm
+++ lib/App/Mist/Util/StripPod.pm
@@ -6,7 +6,7 @@ use strict;
 
 use base ('Pod::Simple');
 
-our $VERSION = "0.47";
+our $VERSION = "0.48";
 
 sub new {
   my $new = shift->SUPER::new(@_);
